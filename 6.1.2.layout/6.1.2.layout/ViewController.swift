9bc32ca5e2a75672b9baa0a93a16b626bbadba5c
student <student@imac-25.local>
2018-12-15 10:44:47 +0800

    just commit

---
+++ 6.1.2.layout/6.1.2.layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  6.1.2.layout
+//
+//  Created by student on 2018/12/15.
+//  Copyright © 2018年 xtx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
